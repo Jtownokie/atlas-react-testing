6ac0ca4bddc9ccb7bd0566ef4e41fe683093bac7
Jtownokie <christopher.stephens@atlasschool.com>
2024-09-11 13:42:42 -0500

    Cloned repo, setup Vitest, ran first snapshot test to verify

---
+++ src/__tests__/CoverArt.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { expect, test } from "vitest";
+import CoverArt from "../components/CoverArt";
+
+test("Cover Art renders correctly", () => {
+  const { container } = render(<CoverArt />);
+  expect(container).toMatchSnapshot();
+});
